ea4f5088764ca7a006e242c62266860b1fa00425
Saul Dominguez <dguezs@gmail.com>
2025-11-15 14:38:10 -0600

    feat: fixing operation

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const {suma, resta} = require('./app');
+const {suma, resta, multiplicacion} = require('./app');
 
 test('suma 1 + 2 = 3',()=>{
     expect(suma(1, 2)).toBe(3)
@@ -6,4 +6,8 @@ test('suma 1 + 2 = 3',()=>{
 
 test('resta 7 - 10 = -3',()=>{
     expect(resta(7, 10)).toBe(-3)
+});
+
+test('multiplicacion 7 * 10 = 70',()=>{
+    expect(multiplicacion(7, 10)).toBe(70)
 });
